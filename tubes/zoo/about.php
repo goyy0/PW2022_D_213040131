05f02ba36a47365e5f280ed01f929556aee32621
goyy0 <yogisaputra9319@gmail.com>
2022-06-09 21:17:18 +0700

    upload tugas

---
+++ tubes/zoo/about.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Zoo website</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="description" content="" />
+	<meta name="author" content="http://code4berry.com" />
+	<!-- css -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" />
+	<link href="css/fancybox/jquery.fancybox.css" rel="stylesheet"> 
+	<link href="css/flexslider.css" rel="stylesheet" />
+	<link href="css/style.css" rel="stylesheet" />
+
+</head>
+<body>
+	<div id="wrapper">
+		
+		<!-- start header -->
+		<?php include('includes/header.php');?>
+		<!-- end header -->
+		<section id="inner-headline">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<h2 class="pageTitle">About Us</h2>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="content">
+			<section class="section-padding">
+				<div class="container">
+					<div class="row showcase-section">
+						<div class="col-md-6">
+							<img src="img/dev1.png" alt="showcase image">
+						</div>
+						<div class="col-md-6">
+							<div class="about-text">
+								<h3>Yogi Saputra Manik </h3>
+								<p>My name is Yogi Saputra Manik .I was born in Bandung on the may 2002 .I never been to school in SDN ANGKASA 1 , and continue to SMPN 1 Margahayu , and also never been to school in SMAN 1 Margaasih .</p>
+								<p>	Now, I studying in UNIVERSITAS PASUNDAN, and I actively as amateur web developer</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</section>
+		<?php include('includes/footer.php');?>
+	</div>
+	<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+<!-- javascript
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="js/jquery.js"></script>
+	<script src="js/jquery.easing.1.3.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.fancybox.pack.js"></script>
+	<script src="js/jquery.fancybox-media.js"></script> 
+	<script src="js/jquery.flexslider.js"></script>
+	<script src="js/animate.js"></script>
+	<!-- Vendor Scripts -->
+	<script src="js/modernizr.custom.js"></script>
+	<script src="js/jquery.isotope.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/animate.js"></script>
+	<script src="js/custom.js"></script>
+</body>
+</html>
---
+++ tubes/zoo/includes/footer.php
@@ -0,0 +1,49 @@
+<footer>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6 col-sm-6">
+							<div class="widget">
+								<h5 class="widgetheading">Our Contact</h5>
+								<address>
+									<strong>YOGS company</strong><br>
+									15, Street<br>
+								Saluyu Margahayu.</address>
+								<p>
+									<i class="icon-phone"></i> +62 821 2344 8745 <br>
+									<i class="icon-envelope-alt"></i> yogisaputra9319@gmail.com
+								</p>
+							</div>
+						</div>
+						<div class="col-md-push-4 col-sm-6">
+							<div class="widget">
+								<h5 class="widgetheading">Quick Links</h5>
+								<ul class="link-list">
+									<li><a href="index.php">Home</a></li>
+									<li><a href="portfolio.php">Gallery</a></li>
+									<li><a href="animals.php">Animals</a></li>
+									<li><a href="about.php">About</a></li>
+									<li><a href="contact.php">Contact us</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div id="sub-footer">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-6">
+								
+							</div>
+							<div class="col-lg-6">
+								<ul class="social-network">
+									<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+									<li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
+									<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
